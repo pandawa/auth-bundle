158e85ab454dab6914744d9b5f917606e983d22b
pandawabot <dev@pandawa.io>
2022-09-09 13:40:27 +0000

    chore: various updates

--- Plugin/ImportPolicyAnnotationPlugin.php
+++ Plugin/ImportPolicyAnnotationPlugin.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Pandawa\Bundle\AuthBundle\Plugin;
 
-use Pandawa\Annotations\Auth\UsePolicy;
+use Illuminate\Contracts\Auth\Access\Gate;
+use Pandawa\Annotations\Auth\AsPolicy;
 use Pandawa\Bundle\AnnotationBundle\Plugin\AnnotationPlugin;
 use Pandawa\Bundle\AuthBundle\Annotation\PolicyLoadHandler;
+use Pandawa\Bundle\AuthBundle\AuthBundle;
 
 /**
  * @author  Iqbal Maulana <iq.bluejack@gmail.com>
@@ -15,13 +17,32 @@ class ImportPolicyAnnotationPlugin extends AnnotationPlugin
 {
     protected ?string $defaultPath = 'Policy';
 
+    public function boot(): void
+    {
+        $config = $this->bundle->getService('config');
+
+        foreach ($config->get($this->getConfigKey(), []) as $model => $policy) {
+            $this->gate()->policy($model, $policy);
+        }
+    }
+
     protected function getAnnotationClasses(): array
     {
-        return [UsePolicy::class];
+        return [AsPolicy::class];
     }
 
     protected function getHandler(): string
     {
         return PolicyLoadHandler::class;
+    }
+
+    protected function gate(): Gate
+    {
+        return $this->bundle->getService(Gate::class);
+    }
+
+    protected function getConfigKey(): string
+    {
+        return AuthBundle::POLICY_CONFIG_KEY . '.' . $this->bundle->getName();
     }
 }
